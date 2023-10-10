61376f1a35f14973ee679d56467aa40d43a0b941
Arthur Migdal <amigdal@mit.edu>
2023-10-09 23:58:28 -0400

    Just a test.
    Nothing useful yet.

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Just a test flake.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
